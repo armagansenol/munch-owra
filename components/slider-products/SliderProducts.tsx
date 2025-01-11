da852291f0a7454553f54eedde4b3c3e3ed0ed5b
armagansenol <armagansnl@gmail.com>
2025-01-11 12:21:13 +0300

    feat: Introduce new IconWavyBg component and update SliderProducts with enhanced marquee functionality. Modify slider styles for improved layout and responsiveness, including adjustments to image dimensions and button styles. Add new product images and remove outdated assets for better visual representation.

--- components/slider-products/SliderProducts.tsx
+++ components/slider-products/SliderProducts.tsx
@@ -1,98 +1,94 @@
 import s from "./slider-products.module.scss"
 
-import { OrthographicCamera } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
 import cx from "clsx"
-import { Leva } from "leva"
-import { useLocale, useTranslations } from "next-intl"
+import { useLocale } from "next-intl"
 import { useEffect, useRef, useState } from "react"
 
 import Float from "@/components/animations/float"
+import { Marquee } from "@/components/animations/marquee"
+import CursorText from "@/components/cursor/cursor-text"
+import { IconArrow, IconWavyBg } from "@/components/icons"
 import { SliderFade } from "@/components/slider-fade"
 import { Img } from "@/components/utility/img"
 import { Link } from "@/components/utility/link"
-import { Vortex } from "@/components/vortex"
-import CursorText from "../cursor/cursor-text"
 import { routes } from "@/lib/constants"
 import { Locales } from "@/types"
 
 export default function SliderProducts() {
-  const t = useTranslations("productSlider")
-
-  const seqs = {
-    iceGlass: "ice-glass",
-    boba: "boba",
-    coffee: "coffee",
-  }
-
   const [currentSlide, setCurrentSlide] = useState(0)
 
-  const handleSelectSlide = (index: number) => {
-    setCurrentSlide(index)
+  const products = {
+    nutrifusion: {
+      type: "nutrifusion",
+      color: "var(--red-dit)",
+      marquee: "NUTRIFUSION",
+    },
+    freshburst: {
+      type: "freshburst",
+      color: "var(--appetizing-asparagus)",
+      marquee: "FRESHBURST",
+    },
+    royalcrisp: {
+      type: "royalcrisp",
+      color: "var(--pico-orange)",
+      marquee: "ROYALCRISP",
+    },
+    chocoshell: {
+      type: "chocoshell",
+      color: "var(--magentle)",
+      marquee: "CHOCO SHELL",
+    },
   }
 
   return (
-    <div className={cx(s.sliderProducts, "w-full h-full flex items-center justify-center")}>
-      <div className={cx(s.sliderC, "z-20")}>
-        <SliderFade onSelectSlide={handleSelectSlide}>
-          <div className={cx(s.slide, "flex flex-col items-center")}>
-            <Float amountY={[-3, 3]}>
-              <div className={cx(s.imgC, "scale-110")}>{<Sequence type={seqs.iceGlass} />}</div>
-            </Float>
-            <div className={s.text}>
-              <h3 className={s.title}>{t("s1.title")}</h3>
+    <div className={s.sliderProductsC} onClick={() => setCurrentSlide((prev) => (prev + 1) % 4)}>
+      <div className={cx(s.sliderProducts, "relative w-full h-full flex items-center justify-center")}>
+        <div className={cx(s.sliderC, "relative z-20")}>
+          <SliderFade>
+            <div className={cx(s.slide, "flex flex-col items-center")}>
+              <Float amountY={[-3, 3]}>
+                <div className={cx(s.imgC, "scale-110")}>
+                  {<Sequence type={Object.values(products)[currentSlide].type} />}
+                </div>
+              </Float>
             </div>
+          </SliderFade>
+        </div>
+        <div className={cx(s.prevBtn, "flex items-center justify-center cursor-pointer")}>
+          <div className={s.iconC}>
+            <IconArrow fill={Object.values(products)[currentSlide].color} rotate={180} />
           </div>
-          {/* <div className={cx(s.slide, "flex flex-col items-center")}>
-            <Float amountY={[-3, 3]}>
-              <div className={cx(s.imgC, "scale-105")}>{<Sequence type={seqs.boba} />}</div>
-            </Float>
-            <div className={s.text}>
-              <h3 className={s.title}>{t("s2.title")}</h3>
-            </div>
+        </div>
+        <div className={cx(s.nextBtn, "flex items-center justify-center cursor-pointer")}>
+          <div className={s.iconC}>
+            <IconArrow fill={Object.values(products)[currentSlide].color} />
           </div>
-          <div className={cx(s.slide, "flex flex-col items-center")}>
-            <Float amountY={[-3, 3]}>
-              <div className={cx(s.imgC, "scale-125")}>{<Sequence type={seqs.coffee} />}</div>
-            </Float>
-            <div className={s.text}>
-              <h3 className={s.title}>{t("s3.title")}</h3>
-            </div>
-          </div> */}
-        </SliderFade>
+        </div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 p-10 z-0">
+          <IconWavyBg fill={Object.values(products)[currentSlide].color} />
+        </div>
       </div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 z-0">
-        <Canvas dpr={2}>
-          <Vortex currentItem={currentSlide} />
-          <OrthographicCamera makeDefault zoom={50} near={0.1} far={1000} position={[0, 0, 100]} />
-          <Leva hidden />
-        </Canvas>
-      </div>
-      {/* <Link
-        className={cx(
-          s.cta,
-          "absolute bottom-auto tablet:bottom-5 right-2 tablet:right-5 top-2 tablet:top-auto flex items-center gap-2 cursor-pointer"
-        )}
-        href="/showcase"
+      <div
+        className={s.marqueeC}
+        style={{ "--text-color": Object.values(products)[currentSlide].color } as React.CSSProperties}
       >
-        <span>{t("cta.title")}</span>
-        <span className={s.iconC}>
-          <div className="w-full h-full">
-            <IconArrow fill="var(--cedar-wood-finish)" />
+        <Marquee repeat={5}>
+          <div className="flex items-center">
+            <h2>{Object.values(products)[currentSlide].marquee}</h2>
           </div>
-        </span>
-      </Link> */}
+        </Marquee>
+      </div>
     </div>
   )
 }
 
-export function Sequence({ type }: { type: unknown }) {
+export function Sequence({ type }: { type: string }) {
   const locale = useLocale()
   const PHASE = 22
 
   const [currentItem, setCurrentItem] = useState(0)
 
-  const intervalRef = useRef<NodeJS.Timeout | null>(null) // Reference to store the interval ID
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
 
   const startLoop = () => {
     if (intervalRef.current) return
@@ -129,7 +125,7 @@ export function Sequence({ type }: { type: unknown }) {
                   priority={true}
                   className="object-contain"
                   alt="Product Visual"
-                  src={`/img/sequences/${type}/s_${i}.png`}
+                  src={`/img/sequences/${type}/${i + 1}.png`}
                   height={1000}
                   width={1000}
                 />
